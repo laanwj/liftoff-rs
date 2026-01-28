89d84e34b11aa1a05789ff5ba1d4405254076377
Mara van der Laan <126646+laanwj@users.noreply.github.com>
2026-01-28 23:29:15 +0100

    lib: Split off function per telemetry packet

--- liftoff-lib/src/crsf_tx.rs
+++ liftoff-lib/src/crsf_tx.rs
@@ -2,101 +2,122 @@ use crate::crsf::{self, CrsfPacket, build_packet};
 use crate::geo;
 use crate::telemetry::TelemetryPacket;
 
+fn build_gps_packet(rec: &TelemetryPacket) -> Option<Vec<u8>> {
+    let position = rec.position?;
+    let attitude = rec.attitude?;
+    let velocity = rec.velocity?;
+
+    let (lon, lat, alt) = geo::gps_from_coord(
+        &[position[0] as f64, position[1] as f64, position[2] as f64],
+        (0.0, 0.0),
+    );
+    let hdg = geo::quat2heading(
+        attitude[0] as f64,
+        attitude[1] as f64,
+        attitude[2] as f64,
+        attitude[3] as f64,
+    );
+    let mut hdg_deg = hdg.to_degrees();
+    if hdg_deg < 0.0 {
+        hdg_deg += 360.0;
+    }
+
+    let vel2d = (velocity[0].powi(2) + velocity[2].powi(2)).sqrt();
+
+    let gps = crsf::Gps {
+        lat: (lat * 10_000_000.0) as i32,
+        lon: (lon * 10_000_000.0) as i32,
+        speed: (vel2d * 3.6 * 10.0) as u16,
+        heading: (hdg_deg * 100.0) as u16,
+        alt: (alt + 1000.0) as u16,
+        sats: 1,
+    };
+    build_packet(&CrsfPacket::Gps(gps))
+}
+
+fn build_battery_packet(rec: &TelemetryPacket) -> Option<Vec<u8>> {
+    let bat = rec.battery?;
+    let battery = crsf::Battery {
+        voltage: (bat[1] * 10.0) as u16,
+        current: 0,
+        capacity: 0,
+        remaining: (bat[0] * 100.0) as u8,
+    };
+    build_packet(&CrsfPacket::Battery(battery))
+}
+
+fn build_vario_packet(rec: &TelemetryPacket) -> Option<Vec<u8>> {
+    let velocity = rec.velocity?;
+    let vario = crsf::Vario {
+        vertical_speed: (velocity[1] * 100.0) as i16,
+    };
+    build_packet(&CrsfPacket::Vario(vario))
+}
+
+fn build_attitude_packet(rec: &TelemetryPacket) -> Option<Vec<u8>> {
+    let attitude = rec.attitude?;
+    let (pitch, roll, yaw) = geo::quat2eulers(
+        attitude[0] as f64,
+        attitude[1] as f64,
+        attitude[2] as f64,
+        attitude[3] as f64,
+    );
+    let att = crsf::Attitude {
+        pitch: (pitch * 10_000.0) as i16,
+        roll: (roll * 10_000.0) as i16,
+        yaw: (yaw * 10_000.0) as i16,
+    };
+    build_packet(&CrsfPacket::Attitude(att))
+}
+
+fn build_baro_alt_packet(rec: &TelemetryPacket) -> Option<Vec<u8>> {
+    let position = rec.position?;
+    let (_lon, _lat, alt) = geo::gps_from_coord(
+        &[position[0] as f64, position[1] as f64, position[2] as f64],
+        (0.0, 0.0),
+    );
+
+    let mut alt_packed = ((alt * 10.0) as i32) + 10000;
+    if alt_packed < 0 {
+        alt_packed = 0;
+    } else if alt_packed > 0x7fff {
+        alt_packed = 0x8000 | (alt as i32).min(0x7fff);
+    }
+
+    let baro = crsf::BaroAlt {
+        alt: alt_packed as u16,
+        vertical_speed: 0,
+    };
+    build_packet(&CrsfPacket::BaroAlt(baro))
+}
+
+fn build_airspeed_packet(rec: &TelemetryPacket) -> Option<Vec<u8>> {
+    let velocity = rec.velocity?;
+    let vel3d = (velocity[0].powi(2) + velocity[1].powi(2) + velocity[2].powi(2)).sqrt();
+    let airspeed = crsf::Airspeed {
+        speed: (vel3d * 3.6 * 10.0) as u16,
+    };
+    build_packet(&CrsfPacket::Airspeed(airspeed))
+}
+
+fn build_rpm_packet(rec: &TelemetryPacket) -> Option<Vec<u8>> {
+    let rpms = rec.motor_rpm.as_ref()?;
+    let rpm = crsf::Rpm {
+        source_id: 0,
+        rpms: rpms.iter().map(|&r| r as u32).collect(),
+    };
+    build_packet(&CrsfPacket::Rpm(rpm))
+}
+
 pub fn generate_crsf_telemetry(rec: &TelemetryPacket) -> Vec<Vec<u8>> {
     let mut packets = Vec::new();
-
-    // GPS
-    if let (Some(pos), Some(att), Some(vel)) = (rec.position, rec.attitude, rec.velocity) {
-        let (lon, lat, alt) =
-            geo::gps_from_coord(&[pos[0] as f64, pos[1] as f64, pos[2] as f64], (0.0, 0.0));
-        let hdg = geo::quat2heading(att[0] as f64, att[1] as f64, att[2] as f64, att[3] as f64);
-        let mut hdg_deg = hdg.to_degrees();
-        if hdg_deg < 0.0 {
-            hdg_deg += 360.0;
-        }
-
-        let vel2d = (vel[0].powi(2) + vel[2].powi(2)).sqrt();
-
-        // GPS Packet
-        let gps = crsf::Gps {
-            lat: (lat * 10_000_000.0) as i32,
-            lon: (lon * 10_000_000.0) as i32,
-            speed: (vel2d * 3.6 * 10.0) as u16,
-            heading: (hdg_deg * 100.0) as u16,
-            alt: (alt + 1000.0) as u16,
-            sats: 1,
-        };
-        packets.push(build_packet(&CrsfPacket::Gps(gps)).unwrap());
-    }
-
-    // Battery
-    if let Some(bat) = rec.battery {
-        let battery = crsf::Battery {
-            voltage: (bat[1] * 10.0) as u16,
-            current: 0,
-            capacity: 0,
-            remaining: (bat[0] * 100.0) as u8,
-        };
-        packets.push(build_packet(&CrsfPacket::Battery(battery)).unwrap());
-    }
-
-    // Vario
-    if let Some(vel) = rec.velocity {
-        let vario = crsf::Vario {
-            vertical_speed: (vel[1] * 100.0) as i16,
-        };
-        packets.push(build_packet(&CrsfPacket::Vario(vario)).unwrap());
-    }
-
-    // Attitude
-    if let Some(att) = rec.attitude {
-        let (pitch, roll, yaw) =
-            geo::quat2eulers(att[0] as f64, att[1] as f64, att[2] as f64, att[3] as f64);
-        let attitude = crsf::Attitude {
-            pitch: (pitch * 10_000.0) as i16,
-            roll: (roll * 10_000.0) as i16,
-            yaw: (yaw * 10_000.0) as i16,
-        };
-        packets.push(build_packet(&CrsfPacket::Attitude(attitude)).unwrap());
-    }
-
-    // Baro Alt
-    if let Some(pos) = rec.position {
-        let (_lon, _lat, alt) =
-            geo::gps_from_coord(&[pos[0] as f64, pos[1] as f64, pos[2] as f64], (0.0, 0.0));
-
-        let mut alt_packed = ((alt * 10.0) as i32) + 10000;
-        if alt_packed < 0 {
-            alt_packed = 0;
-        } else if alt_packed > 0x7fff {
-            alt_packed = 0x8000 | (alt as i32).min(0x7fff);
-        }
-
-        let baro = crsf::BaroAlt {
-            alt: alt_packed as u16,
-            vertical_speed: 0,
-        };
-        packets.push(build_packet(&CrsfPacket::BaroAlt(baro)).unwrap());
-    }
-
-    // Airspeed
-    if let Some(vel) = rec.velocity {
-        let vel3d = (vel[0].powi(2) + vel[1].powi(2) + vel[2].powi(2)).sqrt();
-        let airspeed = crsf::Airspeed {
-            speed: (vel3d * 3.6 * 10.0) as u16,
-        };
-        packets.push(build_packet(&CrsfPacket::Airspeed(airspeed)).unwrap());
-    }
-
-    // RPM
-    if let Some(rpms) = &rec.motor_rpm {
-        let rpm = crsf::Rpm {
-            source_id: 0,
-            rpms: rpms.iter().map(|&r| r as u32).collect(),
-        };
-        packets.push(build_packet(&CrsfPacket::Rpm(rpm)).unwrap());
-    }
-
+    packets.extend(build_gps_packet(rec));
+    packets.extend(build_battery_packet(rec));
+    packets.extend(build_vario_packet(rec));
+    packets.extend(build_attitude_packet(rec));
+    packets.extend(build_baro_alt_packet(rec));
+    packets.extend(build_airspeed_packet(rec));
+    packets.extend(build_rpm_packet(rec));
     packets
 }
 
